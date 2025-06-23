740fe75b7ead9f54553ce0436f582d0161832c4b
Mahmoud <mahmoudhmad411@gmail.com>
2025-06-23 05:02:25 +0300

    new url for backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -42,7 +42,7 @@ const getBaseUrl = () => {
       return process.env.NEXT_PUBLIC_API_BASE_URL;
     }
     // قيمة افتراضية إذا لم يتم العثور على المتغير
-    return 'https://23fa-41-36-35-172.ngrok-free.app';
+    return 'https://f66a-41-36-35-172.ngrok-free.app';
   };
   
   const handleResponse = async (response) => {
