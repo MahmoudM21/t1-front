9e666a9f2fd344e76b4dff77f2bb323d29d43c01
Mahmoud <mahmoudhmad411@gmail.com>
2025-06-29 01:51:58 +0300

    new token handling front

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -11,6 +11,21 @@ import { Checkbox } from "@/components/ui/checkbox";
 import ThemeToggle from '@/components/ThemeToggle';
 import { apiPost } from '@/utils/api';
 
+function getCSRFToken() {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          if (cookie.substring(0, 10) === 'csrftoken=') {
+              cookieValue = decodeURIComponent(cookie.substring(10));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
 const SignupPage = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
